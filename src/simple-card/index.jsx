efa9336013912f83e8cde56e9b9fad4aa70b3abd
Steven <stevenjamescook@gmail.com>
2017-03-19 08:26:49 +0000

    Added some styles

--- src/simple-card/index.jsx
+++ src/simple-card/index.jsx
@@ -4,8 +4,14 @@ class SimpleCard extends React.Component {
 
     render() {
         return (
-            <div className="col-md-4 col-sm-12 simple-card" >
-                Simple Card
+            <div className="col-md-4 col-sm-12 simple-card">
+                <a href="/waters/loch-neldricken">
+                <img src="https://dur8xuaowfaya.cloudfront.net/images/images/000/000/077/medium/neldricken.jpg?1476951599" className="img-responsive img-centered" alt="Loch Neldricken" />
+                <div className="caption">
+                    <h4>Loch Neldricken</h4>
+                    <p className="text-muted">Galloway Forest Park, Scotland</p>
+                </div>
+                </a>
             </div>
         );
     }
